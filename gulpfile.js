861bdc5f408cfe6c83f1aa8639a68756588e5a28
daukadolt <daulet.amirkhanov.official@gmail.com>
2020-06-10 15:33:52 +0600

    gulp setup for linting and building in npm "pretest"
    1. Tests have also been taken out of src since tests now use
       "dist" files instead of "src"

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const ts = require('gulp-typescript');
+const del = require('del');
+
+const { src, dest, task } = gulp;
+const tsProject = ts.createProject('tsconfig.json');
+
+task('eslint', () => {
+    return src(['src/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+task('build', () => {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(dest('dist'));
+});
+
+task('clean', () => {
+    return del('./dist');
+});
+
+task('default', gulp.series('eslint', 'clean', 'build'));
